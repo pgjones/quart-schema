1a1d001ed59cdeb58535fb625072077b23ee1bd1
pgjones <philip.graham.jones@googlemail.com>
2021-10-10 10:48:10 +0100

    Add a link to the gitlab repo
    Using the official icon.

--- docs/conf.py
+++ docs/conf.py
@@ -52,8 +52,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = "pydata_sphinx_theme"
 
 html_theme_options = {
-  "external_links": [
-      {"name": "Source code", "url": "https://gitlab.com/pgjones/quart-schema"},
-      {"name": "Issues", "url": "https://gitlab.com/pgjones/quart-schema/issues"},
-  ]
+    "external_links": [
+        {"name": "Source code", "url": "https://gitlab.com/pgjones/quart-schema"},
+        {"name": "Issues", "url": "https://gitlab.com/pgjones/quart-schema/issues"},
+    ],
+    "icon_links": [
+        {
+            "name": "GitLab",
+            "url": "https://gitlab.com/pgjones/quart-schema",
+            "icon": "fab fa-gitlab",
+        },
+    ],
 }
